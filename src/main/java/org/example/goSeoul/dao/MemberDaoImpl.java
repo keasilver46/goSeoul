b79f5db856810e50d456b4d814d8124cb2caea4d
potato-5 <130821200+potato-5@users.noreply.github.com>
2023-05-15 14:24:32 +0900

    회원가입  DAO

--- src/main/java/org/example/goSeoul/dao/MemberDaoImpl.java
+++ src/main/java/org/example/goSeoul/dao/MemberDaoImpl.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.lang.reflect.Member;
 
 @Repository
-public class MemberDaoImpl implements MemberDao {
+public class MemberDaoImpl{
 
     @Autowired
     private SqlSession sqlSession;
