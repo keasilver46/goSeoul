13adff15e002f5059eb470eb282cff8caa9df8e8
potato-5 <130821200+potato-5@users.noreply.github.com>
2023-05-15 14:26:08 +0900

    회원가입 서비스

--- src/main/java/org/example/goSeoul/service/MemberServiceImpl.java
+++ src/main/java/org/example/goSeoul/service/MemberServiceImpl.java
@@ -1,16 +1,16 @@
 package org.example.goSeoul.service;
 
-import org.example.goSeoul.dao.MemberDao;
 import org.example.goSeoul.dao.MemberDaoImpl;
 import org.example.goSeoul.model.MemberBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MemberServiceImpl implements MemberService {
+public class MemberServiceImpl{
 
     @Autowired
     private MemberDaoImpl dao;
+    
     public void insert(MemberBean member) throws Exception {
         dao.insert(member);
     }
